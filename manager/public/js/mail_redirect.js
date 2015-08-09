09e2615ec5281b6543f9b70035df6ac4d8b596ae
INOPIAE <inopiae@cacert.org>
2015-08-09 19:13:45 +0200

    bug 1396: Codestyle cleanup

--- manager/public/js/mail_redirect.js
+++ manager/public/js/mail_redirect.js
@@ -2,7 +2,7 @@ var delay = 2000;
 setTimeout('redirect()', delay);
 
 function redirect() {
-	var returnto = document.getElementById('returnto');
-	
-	window.location = returnto.value;
+    var returnto = document.getElementById('returnto');
+
+    window.location = returnto.value;
 }
